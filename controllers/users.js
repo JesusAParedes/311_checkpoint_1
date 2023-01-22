5f8745d46e5ca87bd6c8a1211d7d9c5656f95f99
JesusAParedes <paredesjesus93@gmail.com>
2023-01-22 17:15:35 -0600

    created routes and controllers. everything is working but need to add error handling and get it to work correctly

---
+++ controllers/users.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const users = require('../data/index');
+const newUser = require('../data/sampleUser');
+
+const listUsers = (req,res) => {
+    res.json(users);
+};
+
+// 404 code
+const showUsers = (req, res) => {
+      const requestedUser = users.filter(user => {
+            if(parseInt(req.params.id) === user.id) {
+                console.log('user', user)
+                return user
+            }
+        } 
+    );
+    console.log('requestedUser', requestedUser)
+    if(users.includes(requestedUser)) {
+        res.json(requestedUser)}
+        else {
+            console.log('includes', users.includes(requestedUser))
+            res.status(400).send('User does not exist')}
+    };
+
+const createUsers = (req, res) => {
+    req.body = newUser;
+    console.log(users.length);
+    const user = {
+      "id": users.length + 1,
+      "name": newUser.name,
+      "username": newUser.username,
+      "email": newUser.email,
+      "address": newUser.address,
+      "phone": newUser.phone,
+      "website": newUser.website,
+      "company": newUser.company
+  }
+     users.push(user);
+    res.send(user);
+  };
+
+
+  // 400 code
+const updateUsers = (req,res) =>  {
+    const updatedUser = req.body;
+    users.map(user => {
+      if(parseInt(req.params.id) === user.id) {
+       
+        user.name = updatedUser.name ? updatedUser.name : updatedUser.name,
+        user.username = updatedUser.username ? updatedUser.username : updatedUser.username,
+        user.email = updatedUser.email ? updatedUser.email : updatedUser.email,
+        user.address = updatedUser.address ? updatedUser.address : updatedUser.address,
+        user.phone = updatedUser.phone ? updatedUser.phone : updatedUser.phone,
+        user.website = updatedUser.website ? updatedUser.website : updatedUser.website,
+        user.company = updatedUser.company ? updatedUser.company : updatedUser.company
+        res.json(user);
+      }     
+    }
+    )
+  };
+
+  // 400 code
+const deleteUsers = (req, res) => { 
+   const notDeletedUsers = users.filter(user => {
+      if(parseInt(req.params.id) !== user.id) {
+        return user;
+      } 
+    })
+
+    if(notDeletedUsers) {
+        res.json(notDeletedUsers)
+    } else (res.status(400).send("User doesn't exist"))
+  };
+
+module.exports = {listUsers, showUsers, createUsers, updateUsers, deleteUsers}
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
+const users = require('../data/index');
+const newUser = require('../data/sampleUser');
+const usersController = require('../controllers/users')
 
-router.get('/users', (req,res) => {
-    res.json(users)
-})
+router.get('/users', usersController.listUsers)
+
+router.get('/users/:id', usersController.showUsers) //return 404
+
+router.post('/users', usersController.createUsers)
+
+router.put ('/users/:id', usersController.updateUsers) //return 400
+
+router.delete('/users/:id', usersController.deleteUsers) //return 400
+
+module.exports = router;
